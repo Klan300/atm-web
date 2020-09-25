93c666553c0fdd3c5bae221569020edf20f8beb0
klan300 <nathun.thun@hotmail.com>
2020-09-25 16:00:54 +0700

    add bankaccount

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,25 +1,42 @@
 package th.ac.ku.atm.service;
 
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
-import th.ac.ku.atm.model.Customer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
 
-    private ArrayList<BankAccount> BankAccountList = new ArrayList<>();
+    private  List<BankAccount> bankAccountList;
+    private RestTemplate restTemplate;
+
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
+    }
+
 
     public void createBankAccount(BankAccount bankAccount) {
-        BankAccountList.add(bankAccount);
+        bankAccountList.add(bankAccount);
     }
 
     public List<BankAccount> getBankAccounts() {
-        return new ArrayList<>(BankAccountList);
+        return new ArrayList<>(bankAccountList);
     }
-
-
 
 }
